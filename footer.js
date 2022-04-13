b98f4606cbfa142673e2f936e16f36b4c9cb7469
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-04-13 10:43:14 -0600

    Footer moved to include

---
+++ footer.js
@@ -0,0 +1,179 @@
+let footerHTML=`
+<section id="footerSection">
+    <footer class="pc-footer bg-hg">
+        <section class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <img src="https://user-images.githubusercontent.com/45856706/151230155-9d0bfe38-5b23-4bca-a13d-68070f16c396.png"
+                        alt="Parks Canada logo" class="pc-logo center-block mrgn-tp-xl">
+                </div>
+            </div>
+
+            <div class="row center-block mrgn-tp-lg">
+                <div class="col-xs-12 col-sm-4 center-block mrgn-bttm-md"><a href="#3"
+                        class="btn btn-footer col-xs-12 col-sm-10 col-sm-offset-1">Job opportunities</a></div>
+                <div class="col-xs-12 col-sm-4 center-block mrgn-bttm-md"><a href="#3"
+                        class="btn btn-footer col-xs-12 col-sm-10 col-sm-offset-1">Volunteer</a></div>
+                <div class="col-xs-12 col-sm-4 center-block mrgn-bttm-md"><a href="#3"
+                        class="btn btn-footer col-xs-12 col-sm-10 col-sm-offset-1">Partner</a></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <h2><a href="#noTarget" class="wb-lbx">About the Parks Canada Agency</a></h2>
+                    <ul class="list-unstyled lst-spcd">
+                        <li><a href="#noTarget" class="wb-lbx">Mandate and Charter</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Message from the Chief Executive Officer</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Indigenous relations at Parks Canada</a></li>
+
+                        <li><a href="#noTarget" class="wb-lbx">Strategies, plans, reports</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Transparency</a></li>
+
+                        <li><a href="#noTarget" class="wb-lbx">News releases</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Media room</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Contact Parks Canada</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2>Minister</h2>
+                    <p><a href="#noTarget" class="wb-lbx">The Honourable Steven Guilbeault</a></p>
+                    <small>Minister of Environment and Climate Change</small>
+                    <h2>Follow us</h2>
+
+                    <ul class="list-unstyled list-inline nowrap text-white">
+                        <li><a class="mrgn-rght-md" href="https://www.facebook.com/parkscanada"><span
+                                    class="fab fa-facebook fa-2x" aria-hidden="true"></span><span
+                                    class="wb-inv">Facebook</span></a></li>
+                        <li><a class="mrgn-rght-md" href="https://twitter.com/parkscanada"><span
+                                    class="fab fa-twitter fa-2x" aria-hidden="true"></span><span
+                                    class="wb-inv">Twitter</span></a></li>
+                        <li><a class="mrgn-rght-md" href="https://www.youtube.com/ParksCanada"><span
+                                    class="fab fa-youtube fa-2x" aria-hidden="true"></span><span
+                                    class="wb-inv">YouTube</span></a></li>
+                        <li><a class="mrgn-rght-md" href="https://www.instagram.com/ParksCanada"><span
+                                    class="fab fa-instagram fa-2x" aria-hidden="true"></span><span
+                                    class="wb-inv">YouTube</span></a></li>
+                    </ul>
+                    <ul class="list-unstyled lst-spcd">
+                        <li><a href="#noTarget" class="wb-lbx">Our newsletter</a></li>
+                        <li><a href="#noTarget" class="wb-lbx">Apps</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+
+            <!-- Footer V1 - Pre Feb 14, 2022 -->
+            <!-- <div class="row mrgn-tp-xl mrgn-bttm-xl">
+            <div class="col-md-3 pc-footer-list">
+                <h2><a href="#noTarget" class="wb-lbx">About Parks Canada</a></h2>
+                <ul class="list-unstyled lst-spcd">
+                    <li><a href="#noTarget" class="wb-lbx">Mandate and Charter</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Our Minister</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Message from the CEO</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Indigenous relations</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Partners</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Strategies and plans</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Reports and statistics</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 pc-footer-list">
+                <h2><a href="#noTarget" class="wb-lbx">News</a></h2>
+                <ul class="list-unstyled lst-spcd">
+                    <li><a href="#noTarget" class="wb-lbx">News release</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Media room</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Travel media</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 pc-footer-list">
+                <h2><a href="#noTarget" class="wb-lbx">Transparency</a></h2>
+                <ul class="list-unstyled lst-spcd">
+                    <li><a href="#noTarget" class="wb-lbx">Proactive disclosure</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Access to information and privacy</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Terms and conditions</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Consultations</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 pc-footer-list">
+                <h2><a href="#noTarget" class="wb-lbx">Stay connected</a></h2>
+                <ul class="list-unstyled lst-spcd">
+                    <li><a href="#noTarget" class="wb-lbx">Newsletter</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Apps</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Facebook</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Instagram</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Twitter</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">YouTube</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Social media in Parks Canada places</a></li>
+                    <li><a href="#noTarget" class="wb-lbx">Contact us</a></li>
+                </ul>
+            </div>
+        </div> -->
+        </section>
+    </footer>
+    <section id="noTarget" class="mfp-hide modal-dialog modal-content overlay-def">
+        <header class="modal-header">
+            <h2 class="modal-title">Oops!</h2>
+        </header>
+        <div class="modal-body">
+            <p>We haven't made this page yet.</p>
+        </div>
+    </section>
+    <footer id="wb-info">
+        <div class="landscape">
+            <nav class="container wb-navcurr">
+                <h2 class="wb-inv">About government</h2>
+                <ul class="list-unstyled colcount-sm-2 colcount-md-3">
+                    <li><a href="https://www.canada.ca/en/contact.html">Contact us</a></li>
+                    <li><a href="https://www.canada.ca/en/government/dept.html">Departments and agencies</a></li>
+                    <li><a href="https://www.canada.ca/en/government/publicservice.html">Public service and
+                            military</a></li>
+                    <li><a href="https://www.canada.ca/en/news.html">News</a></li>
+                    <li><a href="https://www.canada.ca/en/government/system/laws.html">Treaties, laws and
+                            regulations</a></li>
+                    <li><a href="https://www.canada.ca/en/transparency/reporting.html">Government-wide reporting</a>
+                    </li>
+                    <li><a href="https://pm.gc.ca/eng">Prime Minister</a></li>
+                    <li><a href="https://www.canada.ca/en/government/system.html">How government works</a></li>
+                    <li><a href="https://open.canada.ca/en/">Open government</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="brand">
+            <!--Start CDTS serverBottom-en -->
+            <div class="container">
+                <div class="row">
+                    <nav class="col-md-9 col-lg-10 ftr-urlt-lnk">
+                        <h2 class="wb-inv">About this site</h2>
+                        <ul>
+                            <li><a href="https://www.canada.ca/en/social.html">Social media</a></li>
+                            <li><a href="https://www.canada.ca/en/mobile.html">Mobile applications</a></li>
+                            <li><a href="https://www1.canada.ca/en/newsite.html">About Canada.ca</a></li>
+                            <li><a href="https://www.canada.ca/en/transparency/terms.html">Terms and conditions</a>
+                            </li>
+                            <li><a href="https://www.canada.ca/en/transparency/privacy.html">Privacy</a></li>
+                        </ul>
+                    </nav>
+                    <div class="col-xs-6 visible-sm visible-xs tofpg"> <a href="#wb-cont">Top of Page <span
+                                class="glyphicon glyphicon-chevron-up"></span></a> </div>
+                    <div class="col-xs-6 col-md-2 text-right"> <img
+                            src="https://www.canada.ca/etc/designs/canada/cdts/gcweb/v4_0_32/assets/wmms-blk.svg"
+                            alt="Symbol of the Government of Canada" />
+                        <meta property="areaServed" typeOf="Country" content="Canada" />
+                        <link property="logo"
+                            href="https://www.canada.ca/etc/designs/canada/cdts/gcweb/v4_0_32/assets/wmms-blk.svg" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--End CDTS serverBottom-en -->
+    <!-- Start Treasury board Script footer -->
+    <script src="https://www.canada.ca/etc.clientlibs/clientlibs/granite/jquery.min.js"></script>
+    <script src="https://www.canada.ca/etc.clientlibs/clientlibs/granite/utils.min.js"></script>
+    <script src="https://www.canada.ca/etc/designs/canada/clientlib-publish.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://www.canada.ca/etc/designs/canada/wet-boew/js/wet-boew.min.js"></script>
+    <script src="https://www.canada.ca/etc/designs/canada/wet-boew/js/theme.min.js"></script>
+    <!-- End Treasury board Script footer -->
+   
+</section>`
