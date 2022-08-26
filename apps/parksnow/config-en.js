163da8047e70793da3bf4501f1697fd05ddb62b2
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 11:31:38 -0600

    Removed styling

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -5,18 +5,20 @@ var wet_boew_geomap = {
              StatusIconFileName: { path: "properties", alias: "Status"}, 
               ModifiedTimestampUtc: { path: "properties", alias: "Time"}, 
               Remarks: { path: "properties", alias: "Remarks"}
-            },
-             style:  { type: "rule", rule: [
-                // 3 = green, 4 = yellow, 5 = red, 6 = black
-
-                { field: "StatusID", value: "greenspot.png",  init: { strokeColor: "#333333", fillColor: "#056C07", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
-                { field: "StatusID", value: "yellowspot.png",  init: { strokeColor: "#333333", fillColor: "#FFF702", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
-                { field: "StatusID", value: "redspot.png",  init: { strokeColor: "#333333", fillColor: "#EB1E25", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
-                { field: "StatusID", value: "blackspot.png",  init: { strokeColor: "#333333", fillColor: "#000000", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}}
-                
-            ]
-        }
+            }
 
         }
     ]
 };
+
+
+        //      style:  { type: "rule", rule: [
+        //         // 3 = green, 4 = yellow, 5 = red, 6 = black
+
+        //         { field: "StatusID", value: "greenspot.png",  init: { strokeColor: "#333333", fillColor: "#056C07", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
+        //         { field: "StatusID", value: "yellowspot.png",  init: { strokeColor: "#333333", fillColor: "#FFF702", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
+        //         { field: "StatusID", value: "redspot.png",  init: { strokeColor: "#333333", fillColor: "#EB1E25", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}},
+        //         { field: "StatusID", value: "blackspot.png",  init: { strokeColor: "#333333", fillColor: "#000000", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1}}
+                
+        //     ]
+        // }
