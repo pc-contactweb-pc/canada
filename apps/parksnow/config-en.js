50f40f3b6e086a47d29e5f506f98aeb1b6fe667c
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 08:10:09 -0600

    Pointing to correct file

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -1,6 +1,6 @@
 var wet_boew_geomap = {
     overlays: [
-        { title: "JSON (Earthquakes)", caption: "This is a sample dataset loaded from a remote JSON resource, in this case the USGS Earthquakes API.", type: "json", url: "https://pc-contactweb-pc.github.io/canada/parksNowResponse.json", visible: !1, popups: !0, datatable: !0, zoom: !0, root: "features", attributes: { LocationName: { path: "properties", alias: "Title"
+        { title: "JSON (Earthquakes)", caption: "This is a sample dataset loaded from a remote JSON resource, in this case the USGS Earthquakes API.", type: "json", url: "https://pc-contactweb-pc.github.io/canada/apps/parksnow/parksNowResponse.json", visible: !1, popups: !0, datatable: !0, zoom: !0, root: "features", attributes: { LocationName: { path: "properties", alias: "Title"
                 }, StatusID: { path: "properties", alias: "Status"
                 }, ModifiedTimestampUtc: { path: "properties", alias: "Time"
                 }, Remarks: { path: "properties", alias: "Remarks"
