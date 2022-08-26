599835e29f6c7b8452a9250e3842457185c82f73
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 15:27:41 -0600

    More commas

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -24,26 +24,26 @@ var wet_boew_geomap = {
                     greenspot: {
                         externalGraphic: "https://user-images.githubusercontent.com/45856706/186992816-29e52dd6-82e6-400e-96ec-b33c19f94db1.png", 
                         fillOpacity: "1", 
-                        graphicWidth: "25"
+                        graphicWidth: "25",
                     },
                     yellowspot: {
                         externalGraphic: "https://user-images.githubusercontent.com/45856706/186992814-f200d633-6ee5-4a87-9814-5ccdf4857817.png", 
                         fillOpacity: "1", 
-                        graphicWidth: "25"
+                        graphicWidth: "25",
                     },
                     redspot: {
                         externalGraphic: "https://user-images.githubusercontent.com/45856706/186992811-1a98cb21-b546-48b7-98dd-cd7036e00107.png", 
                         fillOpacity: "1", 
-                        graphicWidth: "25"
+                        graphicWidth: "25",
                     },
                     blackspot: {
                         externalGraphic: "https://user-images.githubusercontent.com/45856706/186992817-b3ede806-f39a-4c71-9089-a7e72d973e5c.png", 
                         fillOpacity: "1", 
-                        graphicWidth: "25"
+                        graphicWidth: "25",
                     },
                 },
                 select: {
-                    externalGraphic: "https://user-images.githubusercontent.com/45856706/186975883-0fcbbc67-3f4e-42f6-9714-eafa6cbf5e3c.png", fillOpacity: "1", graphicWidth: "25"
+                    externalGraphic: "https://user-images.githubusercontent.com/45856706/186975883-0fcbbc67-3f4e-42f6-9714-eafa6cbf5e3c.png", fillOpacity: "1", graphicWidth: "25",
                 },
             },
 
