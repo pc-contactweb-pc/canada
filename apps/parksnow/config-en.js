5c8c5ce807aceb6af2bcad3701bc6d97ddd8d90f
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 15:26:22 -0600

    Switch to coloured

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -22,17 +22,22 @@ var wet_boew_geomap = {
                 field: "Type", 
                 init: {
                     greenspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186975873-c7b4ee62-8ba2-418a-8aaf-a89f6c4c3d14.png", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992816-29e52dd6-82e6-400e-96ec-b33c19f94db1.png", 
                         fillOpacity: "1", 
                         graphicWidth: "25"
                     },
                     yellowspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186975873-c7b4ee62-8ba2-418a-8aaf-a89f6c4c3d14.png", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992814-f200d633-6ee5-4a87-9814-5ccdf4857817.png", 
                         fillOpacity: "1", 
                         graphicWidth: "25"
                     },
                     redspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186975873-c7b4ee62-8ba2-418a-8aaf-a89f6c4c3d14.png", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992811-1a98cb21-b546-48b7-98dd-cd7036e00107.png", 
+                        fillOpacity: "1", 
+                        graphicWidth: "25"
+                    },
+                    blackspot: {
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992817-b3ede806-f39a-4c71-9089-a7e72d973e5c.png", 
                         fillOpacity: "1", 
                         graphicWidth: "25"
                     },
