cee4c7214341b99f490820e7a090db1f90a97089
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 11:56:19 -0600

    Removed duplicate init

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -12,13 +12,12 @@ var wet_boew_geomap = {
                     // 3 = green, 4 = yellow, 5 = red, 6 = black
 
 
-                    "greenspot.png": { name: "Available", init: { strokeColor: "#333333", fillColor: "#056C07", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 } },
-                    "yellowspot.png": { name: "Limited", init: { strokeColor: "#333333", fillColor: "#FFF702", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 } },
-                    "redspot.png": { name: "Full", init: { strokeColor: "#333333", fillColor: "#EB1E25", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 } },
-                    "blackspot.png": { name: "Closed", init: { strokeColor: "#333333", fillColor: "#000000", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 } }
+                    "greenspot.png": { name: "Available", strokeColor: "#333333", fillColor: "#056C07", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 },
+                    "yellowspot.png": { name: "Limited", strokeColor: "#333333", fillColor: "#FFF702", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 },
+                    "redspot.png": { name: "Full", strokeColor: "#333333", fillColor: "#EB1E25", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 },
+                    "blackspot.png": { name: "Closed", strokeColor: "#333333", fillColor: "#000000", pointRadius: 2.5, fillOpacity: .8, strokeWidth: 1 }
                 }
             }
-
         }
     ]
 };
