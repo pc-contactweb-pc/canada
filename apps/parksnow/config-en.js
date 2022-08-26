5de074aac76d5c85d448293939d6a7d3e54a0e7c
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 15:39:16 -0600

    Select simplified to graphic Opacity

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -47,7 +47,7 @@ var wet_boew_geomap = {
                     },
                 },
                 select: {
-                    externalGraphic: "https://user-images.githubusercontent.com/45856706/186975883-0fcbbc67-3f4e-42f6-9714-eafa6cbf5e3c.png", fillOpacity: "1", graphicWidth: "25",
+                    externalGraphic: "https://user-images.githubusercontent.com/45856706/186975883-0fcbbc67-3f4e-42f6-9714-eafa6cbf5e3c.png", graphicOpacity: "1",
                 },
             },
         },
