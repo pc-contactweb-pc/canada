97f5ea5ea90767446dd27b85c51f159f68141e7d
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-26 08:01:34 -0600

    Creation of Config-en and move of ParksNowResponse to /apps/

---
+++ apps/parksnow/config-en.js
@@ -0,0 +1,11 @@
+var wet_boew_geomap = {
+    overlays: [
+        { title: "JSON (Earthquakes)", caption: "This is a sample dataset loaded from a remote JSON resource, in this case the USGS Earthquakes API.", type: "json", url: "https://pc-contactweb-pc.github.io/canada/parksNowResponse.json", visible: !1, popups: !0, datatable: !0, zoom: !0, root: "features", attributes: { LocationName: { path: "properties", alias: "Title"
+                }, StatusID: { path: "properties", alias: "Status"
+                }, ModifiedTimestampUtc: { path: "properties", alias: "Time"
+                }, Remarks: { path: "properties", alias: "Remarks"
+            }
+            }
+        }
+    ]
+};
