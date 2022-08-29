5183c9b25ed3796462a2cbfbf5021c2498bee639
DMcDona <45856706+DMcDona@users.noreply.github.com>
2022-08-29 12:57:02 -0600

    Test of adding basemap config

--- apps/parksnow/config-en.js
+++ apps/parksnow/config-en.js
@@ -1,45 +1,64 @@
 var wet_boew_geomap = {
+    basemap: {
+        title: 'CBMT',
+        type: 'wms',
+        url: 'https://geogratis.gc.ca/maps/CBMT',
+        layers: 'CBMT',
+        format: 'image/png',
+        version: '1.1.1',
+        options: { singleTile: true, ratio: 1.0, projection: 'EPSG:3978', fractionalZoom: true },
+        mapOptions: {
+            maxExtent: '-3000000.0, -800000.0, 4000000.0, 3900000.0',
+            maxResolution: 'auto',
+            projection: 'EPSG:3978',
+            restrictedExtent: '-3000000.0, -800000.0, 4000000.0, 3900000.0',
+            units: 'm',
+            displayProjection: 'EPSG:4269',
+            numZoomLevels: 12
+        }
+    },
     overlays: [
         {
-            title: "Parks Now", 
-            caption: "The latest information on visiting Parks Canada", 
-            type: "json", 
-            url: "https://pc-contactweb-pc.github.io/canada/apps/parksnow/parksNowResponse.json?3", 
-            visible: !0, 
-            popups: !0, 
-            datatable: !0, 
-            zoom: !0, 
-            root: "features", 
+            title: "Parks Now",
+            caption: "The latest information on visiting Parks Canada",
+            type: "json",
+            url: "https://pc-contactweb-pc.github.io/canada/apps/parksnow/parksNowResponse.json?3",
+            visible: !0,
+            popups: !0,
+            datatable: !0,
+            zoom: !0,
+            root: "features",
             attributes: {
                 StatusIconFileName: { path: "properties", alias: "Type" },
                 LocationName: { path: "properties", alias: "Location" },
                 ModifiedTimestampUtc: { path: "properties", alias: "Updated" },
-                Remarks: { path: "properties", alias: "Remarks" } },
+                Remarks: { path: "properties", alias: "Remarks" }
+            },
             style: {
-                type: "unique", 
-                field: "Type", 
+                type: "unique",
+                field: "Type",
                 init: {
                     greenspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992816-29e52dd6-82e6-400e-96ec-b33c19f94db1.png", 
-                        fillOpacity: "1", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992816-29e52dd6-82e6-400e-96ec-b33c19f94db1.png",
+                        fillOpacity: "1",
                         graphicWidth: "25",
                         name: "Parking available",
                     },
                     yellowspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992814-f200d633-6ee5-4a87-9814-5ccdf4857817.png", 
-                        fillOpacity: "1", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992814-f200d633-6ee5-4a87-9814-5ccdf4857817.png",
+                        fillOpacity: "1",
                         graphicWidth: "25",
                         name: "Limited availability",
                     },
                     redspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992811-1a98cb21-b546-48b7-98dd-cd7036e00107.png", 
-                        fillOpacity: "1", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992811-1a98cb21-b546-48b7-98dd-cd7036e00107.png",
+                        fillOpacity: "1",
                         graphicWidth: "25",
                         name: "Parking full",
                     },
                     blackspot: {
-                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992817-b3ede806-f39a-4c71-9089-a7e72d973e5c.png", 
-                        fillOpacity: "1", 
+                        externalGraphic: "https://user-images.githubusercontent.com/45856706/186992817-b3ede806-f39a-4c71-9089-a7e72d973e5c.png",
+                        fillOpacity: "1",
                         graphicWidth: "25",
                         name: "Parking closed",
                     }
