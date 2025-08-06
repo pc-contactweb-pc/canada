74eed8e78e7cc2c9fda981b9d13896678ef07e1d
Drew McDonald <drew.mcdonald@pc.gc.ca>
2025-08-06 16:50:56 -0600

    Added themes-dist-GCWeb

---
+++ wet-boew/js/ie8-wet-boew.js
@@ -0,0 +1,10 @@
+/*!
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * v4.0.89 - 2025-08-05
+ *
+ *//*
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ */
+/*! Deprecated */
