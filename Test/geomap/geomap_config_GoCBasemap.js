82e6285ab9856bcda46829cb4ac43272aff7e1d6
DMcDona <45856706+DMcDona@users.noreply.github.com>
2025-08-07 10:57:38 -0600

    Attempt 1 at map - https://open.canada.ca/data/en/dataset/296de17c-001c-4435-8f9a-f5acab632e85/resource/2c018f70-4546-4fd6-bfbd-7ba4b4290b5b

--- Test/geomap/geomap_config_GoCBasemap.js
+++ Test/geomap/geomap_config_GoCBasemap.js
@@ -2,7 +2,7 @@ var wet_boew_geomap = {
 	basemap : {
 		title: "Basic Map",
 		type: "esri",
-		url: "https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/provinces1c/MapServer/export",
+		url: "https://maps-cartes.services.geo.ca/server2_serveur2/rest/services/BaseMaps/CBMT3978/MapServer",
 		options: { singleTile: false, ratio: 1.0, projection: "EPSG:3978", fractionalZoom: true },
 		mapOptions: {
 			maxExtent: "-3000000.0, -800000.0, 4000000.0, 3900000.0",
