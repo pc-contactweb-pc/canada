b96d45cbcb8f4d4fd7b81ee79772b7958779c7d5
DMcDona <45856706+DMcDona@users.noreply.github.com>
2025-08-08 11:11:55 -0600

    Styling test 1

--- Test/geomap/geomap_config_GoCBasemap.js
+++ Test/geomap/geomap_config_GoCBasemap.js
@@ -48,8 +48,135 @@ var wet_boew_geomap = {
             caption: "National Parks, Reserves & Marine Conservation Areas administered by Parks Canada",
             type: "geojson",
             url: "https://opendata.arcgis.com/datasets/0fb235ee5bb34e51a825add061dd1a21_0.geojson",
-            visible: true
+            visible: true,
+            attributes: {
+                PLACE_TYPE_E: { path: "properties", alias: "Type" },
+                DESC_EN: { path: "properties", alias: "Title" }
+            },
+            style: {
+                type: "rule",
+                rule: [
+                    {
+                        field: "Type",
+                        value: ["National Park"],
+                        filter: "EQUAL_TO",
+                        name: "National Park",
+                        init: {
+                            strokeColor: "#0c3817ff",
+                            fillColor: "#125e25ff",
+                            fillOpacity: 0.8,
+                            strokeWidth: 1
+                        },
+                    }
+                ]
+            }
         }
 
     ]
 };
+//{"id" : "points-table-blank-example","zoom" : true,"tooltips" : true,"tooltipText" : "Name","popups" : true,"popupsInfo" : { "content" : "<h4>_Name</h4><p>_Description</p>" },"style" : { "type" : "rule", "rule" : [{ "field": "Category", "value": ["Location 1"], "filter": "EQUAL_TO", "init": { "externalGraphic": "https://parks.canada.ca/-/media/WET4/iconographie-icons/carte-map/geomatic/geomap_square-1.png", "graphicWidth": 35, "graphicHeight": 35, "fillOpacity": 1 } },]}}
+
+/*
+{
+      title: "JSON (Earthquakes)",
+      caption:
+        "This is a sample dataset loaded from a remote JSON resource, in this case the USGS Earthquakes API.",
+      type: "json",
+      url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
+      visible: !1,
+      popups: !0,
+      datatable: !0,
+      zoom: !0,
+      root: "features",
+      attributes: {
+        title: { path: "properties", alias: "Title" },
+        mag: { path: "properties", alias: "Magnitude" },
+        time: { path: "properties", alias: "Time" },
+      },
+      style: {
+        type: "rule",
+        rule: [
+          {
+            field: "Magnitude",
+            value: [2],
+            filter: "LESS_THAN",
+            name: "M < 2",
+            init: {
+              strokeColor: "#333333",
+              fillColor: "#000066",
+              pointRadius: 2.5,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+          {
+            field: "Magnitude",
+            value: [2, 2.9],
+            filter: "BETWEEN",
+            name: "M-2",
+            init: {
+              strokeColor: "#333333",
+              fillColor: "#6600cc",
+              pointRadius: 4.5,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+          {
+            field: "Magnitude",
+            value: [3, 3.9],
+            filter: "BETWEEN",
+            name: "M-3",
+            init: {
+              strokeColor: "#333333",
+              fillColor: "#990099",
+              pointRadius: 6.5,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+          {
+            field: "Magnitude",
+            value: [4, 4.9],
+            filter: "BETWEEN",
+            name: "M-4",
+            init: {
+              strokeColor: "#333333",
+              fillColor: "#ff0000",
+              pointRadius: 8,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+          {
+            field: "Magnitude",
+            value: [5, 5.9],
+            filter: "BETWEEN",
+            name: "M-5",
+            init: {
+              graphicName: "star",
+              strokeColor: "#333333",
+              fillColor: "#ff6600",
+              pointRadius: 14,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+          {
+            field: "Magnitude",
+            value: [5.9],
+            filter: "GREATER_THAN",
+            name: "M-6+",
+            init: {
+              graphicName: "star",
+              strokeColor: "#333333",
+              fillColor: "#ff9933",
+              pointRadius: 18,
+              fillOpacity: 0.8,
+              strokeWidth: 1,
+            },
+          },
+        ],
+      },
+    },
+    */
