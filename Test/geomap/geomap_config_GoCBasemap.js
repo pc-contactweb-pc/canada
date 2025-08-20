7a985f4d6ba19fe3cfdc31e7935aacfec7678864
DMcDona <45856706+DMcDona@users.noreply.github.com>
2025-08-20 16:48:02 -0600

    Swapped basemap

--- Test/geomap/geomap_config_GoCBasemap.js
+++ Test/geomap/geomap_config_GoCBasemap.js
@@ -2,8 +2,8 @@ var wet_boew_geomap = {
     basemap: {
         title: 'Basic Map',
         type: 'esri',
-        // url: 'https://maps-cartes.services.geo.ca/server2_serveur2/rest/services/BaseMaps/CBMT3978/MapServer',
-        url: 'https://parks.canada.ca/-/media/19334603EFBA4355BD9DFFBED8280838.ashx',
+        url: 'https://maps-cartes.services.geo.ca/server2_serveur2/rest/services/BaseMaps/CBMT3978/MapServer',
+        // url: 'https://parks.canada.ca/-/media/19334603EFBA4355BD9DFFBED8280838.ashx',
         options: { singleTile: false, ratio: 1.0, projection: 'EPSG:3978', fractionalZoom: true },
         mapOptions: {
             maxExtent: '-3000000.0, -800000.0, 4000000.0, 3900000.0',
