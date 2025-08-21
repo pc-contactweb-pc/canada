17b8142fced41cb7adc9730837100fee19208a2f
DMcDona <45856706+DMcDona@users.noreply.github.com>
2025-08-20 18:14:42 -0600

    Restyled

--- Test/geomap/geomap_config_GoCBasemap.js
+++ Test/geomap/geomap_config_GoCBasemap.js
@@ -17,87 +17,23 @@ var wet_boew_geomap = {
         }
     },
     overlays: [
-
         {
-            title: 'Parks Canada Areas',
-            caption: 'National Parks, Reserves & Marine Conservation Areas administered by Parks Canada',
-            type: 'geojson',
-            url: 'https://opendata.arcgis.com/datasets/0fb235ee5bb34e51a825add061dd1a21_0.geojson',
+            title: "Parks Canada Areas",
+            caption: "National Parks, Reserves & Marine Conservation Areas administered by Parks Canada",
+            type: "geojson",
+            url: "https://opendata.arcgis.com/datasets/0fb235ee5bb34e51a825add061dd1a21_0.geojson",
             visible: true,
             datatable: true,
             attributes: {
-                PLACE_TYPE_E: { path: 'properties', alias: 'Category' },
-                // DESC_EN: { path: 'properties', alias: 'Park_Name' }
+                PLACE_TYPE_E: { path: "properties", alias: "Category" }
+                // DESC_EN: { path: "properties", alias: "Park_Name" }
             },
-
-            // style: {
-            //     type: 'unique',
-            //     field: 'Category',
-            //     init: {
-            //         'National Park': { fillColor: '#125e25', fillOpacity: 0.8, strokeWidth: 1 },
-            //         'National Park Reserve': { fillColor: '#125e25', fillOpacity: 0.8, strokeWidth: 1 },
-            //         'National Marine Conservation Area': { fillColor: '#2816ca', fillOpacity: 0.8, strokeWidth: 1 },
-            //         'National Marine Conservation Area Reserve': { fillColor: '#2816ca', fillOpacity: 0.8, strokeWidth: 1 }
-            //     },
-            //     default: { fillColor: '#666666', fillOpacity: 0.5, strokeWidth: 1 },
-            //     select: { fillColor: '#990000', fillOpacity: 1, strokeWidth: 2 }
-            // }
-
-            // style: {
-            //     type: 'unique',
-            //     field: 'Category',
-            //     init: {
-            //         'National Park': {
-            //             'strokeColor': '#ff0000',
-            //             'fillColor': '#ff0000',
-            //             'fillOpacity': '0.4'
-            //         },
-            //         'National Park Reserve': {
-            //             'strokeColor': '#ff0000',
-            //             'fillColor': '#ff0000',
-            //             'fillOpacity': '0.4'
-            //         },
-            //         'National Marine Conservation Area': {
-            //             'strokeColor': '#ff0000',
-            //             'fillColor': '#ff0000',
-            //             'fillOpacity': '0.4'
-            //         },
-            //         'National Marine Conservation Area Reserve': {
-            //             'strokeColor': '#ff0000',
-            //             'fillColor': '#ff0000',
-            //             'fillOpacity': '0.4'
-            //         }
-            //     },
-            //     select: {
-            //         'strokeColor': '#ff0000',
-            //         'fillColor': '#ff0000',
-            //         'fillOpacity': '0.4'
-            //     }
-            // }
-            // This style works:
-            // style: {
-            //     'strokeColor': '#ff0000',
-            //     'fillColor': '#ff0000',
-            //     'fillOpacity': '0.4'
-            // }
-            // Save us chatGPT:
-            // style: {
-            //     'strokeColor': '#ff0000',
-            //     'fillColor': '#ff0000',
-            //     'fillOpacity': '0.4',
-            //     select: {
-            //         'strokeColor': '#1cd353ff',
-            //         'fillColor': '#b628c9ff',
-            //         'fillOpacity': '0.6'
-            //     }
-            // }
-
             style: {
                 "type": "rule",
                 "rule": [
                     {
                         "field": "Category",
-                        "value": "National Park",
+                        "value": ["National Park"],
                         "filter": "EQUAL_TO",
                         "init": {
                             "strokeColor": "#008000",
@@ -120,7 +56,72 @@ var wet_boew_geomap = {
                     "fillOpacity": "0.6"
                 }
             }
-
         }
     ]
+
+
+    // style: {
+    //     type: 'unique',
+    //     field: 'Category',
+    //     init: {
+    //         'National Park': { fillColor: '#125e25', fillOpacity: 0.8, strokeWidth: 1 },
+    //         'National Park Reserve': { fillColor: '#125e25', fillOpacity: 0.8, strokeWidth: 1 },
+    //         'National Marine Conservation Area': { fillColor: '#2816ca', fillOpacity: 0.8, strokeWidth: 1 },
+    //         'National Marine Conservation Area Reserve': { fillColor: '#2816ca', fillOpacity: 0.8, strokeWidth: 1 }
+    //     },
+    //     default: { fillColor: '#666666', fillOpacity: 0.5, strokeWidth: 1 },
+    //     select: { fillColor: '#990000', fillOpacity: 1, strokeWidth: 2 }
+    // }
+
+    // style: {
+    //     type: 'unique',
+    //     field: 'Category',
+    //     init: {
+    //         'National Park': {
+    //             'strokeColor': '#ff0000',
+    //             'fillColor': '#ff0000',
+    //             'fillOpacity': '0.4'
+    //         },
+    //         'National Park Reserve': {
+    //             'strokeColor': '#ff0000',
+    //             'fillColor': '#ff0000',
+    //             'fillOpacity': '0.4'
+    //         },
+    //         'National Marine Conservation Area': {
+    //             'strokeColor': '#ff0000',
+    //             'fillColor': '#ff0000',
+    //             'fillOpacity': '0.4'
+    //         },
+    //         'National Marine Conservation Area Reserve': {
+    //             'strokeColor': '#ff0000',
+    //             'fillColor': '#ff0000',
+    //             'fillOpacity': '0.4'
+    //         }
+    //     },
+    //     select: {
+    //         'strokeColor': '#ff0000',
+    //         'fillColor': '#ff0000',
+    //         'fillOpacity': '0.4'
+    //     }
+    // }
+    // This style works:
+    // style: {
+    //     'strokeColor': '#ff0000',
+    //     'fillColor': '#ff0000',
+    //     'fillOpacity': '0.4'
+    // }
+    // Save us chatGPT:
+    // style: {
+    //     'strokeColor': '#ff0000',
+    //     'fillColor': '#ff0000',
+    //     'fillOpacity': '0.4',
+    //     select: {
+    //         'strokeColor': '#1cd353ff',
+    //         'fillColor': '#b628c9ff',
+    //         'fillOpacity': '0.6'
+    //     }
+    // }
+
+
+
 };
