cea15df1451b57044dcd482ae02e1264db0be9f1
pc-contactweb-pc <93224537+pc-contactweb-pc@users.noreply.github.com>
2026-02-25 11:22:44 -0800

    test removing map center and zoom defaults

--- location-finder/files/locationSearch.js
+++ location-finder/files/locationSearch.js
@@ -11,8 +11,8 @@
 			],
 			mapOptions: {
 				projection: "EPSG:900913",
-				center: [-90.7222765, 62.5410882],
-				zoomLevel: 3
+				//center: [-90.7222765, 62.5410882],
+				//zoomLevel: 3
 			}
 		}
 };
