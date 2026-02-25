6c5755985283206e72ef0a4f65a8546555651121
pc-contactweb-pc <93224537+pc-contactweb-pc@users.noreply.github.com>
2026-02-25 12:09:32 -0800

    test removing attributes from locationsearch.js

--- location-finder/files/locationSearch.js
+++ location-finder/files/locationSearch.js
@@ -1,14 +1,6 @@
 ﻿var wet_boew_geomap = {
 	basemap: {
 		title: "MapQuest OSM Map",
-			title: 'MapQuest OSM Map',
-			type: "xyz",
-			url: [
-				"http://otile1.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
-				"http://otile2.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
-				"http://otile3.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
-				"http://otile4.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png"
-			],
 			mapOptions: {
 				projection: "EPSG:900913",
 				//center: [-90.7222765, 62.5410882],
