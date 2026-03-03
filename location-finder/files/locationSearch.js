74bf2165db3cb930b4384bea2c07554f463040f3
pc-contactweb-pc <93224537+pc-contactweb-pc@users.noreply.github.com>
2026-03-03 14:30:52 -0800

    updated locationSearch

--- location-finder/files/locationSearch.js
+++ location-finder/files/locationSearch.js
@@ -1,7 +1,12 @@
 ﻿var wet_boew_geomap = {
 	basemap: {
-		title: "MapQuest OSM Map",
-			type: "xyz",
+		title: "OpenStreetMap",
+			type: "osm",
+			url: [
+				"https://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
+				"https://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
+				"https://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+			],
 			mapOptions: {
 				projection: "EPSG:3857",
 			}
